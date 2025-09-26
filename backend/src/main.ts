f6d9d722111a815af15024035d2483f47f5cb61a
Theo-17 <mateo.espinoza@epn.edu.ec>
2025-09-25 21:15:38 -0500

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -48,7 +48,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
+  console.log(`NestJS listening on port ${port}`);
 }
 bootstrap();
